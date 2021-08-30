8372fa56c9aa5c1dbb51b20072785e92a1bc860e
Keshav Bharadwaj <72199479+Keshav-Bharadwaj@users.noreply.github.com>
2021-08-30 12:47:33 +0200

    Evaluation fixes
    Evaluation fixes for varied generated synthetic data

--- evaluation.py
+++ evaluation.py
@@ -1,31 +1,32 @@
 import argparse
 import numpy as np
-import pandas as pd
 from evalution_modules import Eval,Evalcombined
 from sklearn.ensemble import AdaBoostRegressor
-from preprocessing import preprocessingFirstColumn
-import warnings
-warnings.filterwarnings("ignore")
-
+from eval_transform import preprocessingFirstColumnSimilar_train
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--gendata_dir', type=str, default='./pretrained_models/finalSyntheticData.txt', help='Generated synthetic data directory',required=True)
-    parser.add_argument('--output_dir', type=str, default='./pretrained_models/', help='Directory to write temporary files',required=True)
-    parser.add_argument('--sample_data', type=str, default='./example_data/sample_data.txt', help='Original data to be mimced',required=True)
-    parser.add_argument('--n_estimators', type=int, default=125, help='Number of estimators of adaboostmodel',required=True)
-    parser.add_argument('--random_state', type=int, default=42, help='Random state to considered by adaboost model',required=True)
-    parser.add_argument('--bins', type=int, default=50, help='Number of bins for KDE plot', required=True)
+    parser.add_argument('--gendata_dir', type=str, default='./pretrained_models/finalSyntheticData.csv', help='Generated Data directory',required=True)
+    parser.add_argument('--sample_data', type=str, default='./example_data/sample_data.txt', help='Original Data to be mimced',required=True)
+    parser.add_argument('--label_encoderFile', type=str, default='./pretrained_models/label_encoder.pickle',
+                        help='Path of the trained label encoder file', required=True)
+    parser.add_argument('--onehot_encoderFile', type=str, default='./pretrained_models/onehot_encoder.pickle',
+                        help='Path of the trained one hot encoder file', required=True)
+    parser.add_argument('--n_estimators', type=int, default=125, help='Number of estimators of Adaboostmodel',
+                        required=True)
+    parser.add_argument('--random_state', type=int, default=42, help='Random state to considered by adaboost model',
+                        required=True)
+    parser.add_argument('--bins', type=int, default=50, help='Number of bins for KDE plot', required=False)
     parser.add_argument('--pathsave', type=str, default='./pretrained_models/residuals_kdeplot',
-                        help='Path and name to save residuals KDE plot', required=True)
+                        help='Path to save residuals KDE plot', required=False)
     args = parser.parse_args()
 
 
     # Orginal data
-    encodedDataframe = preprocessingFirstColumn(args)
-    X_train = encodedDataframe.iloc[:, :-1].values
-    y_train = encodedDataframe.iloc[:, -1].values
+    df0 = preprocessingFirstColumnSimilar_train(args.label_encoderFile,args.onehot_encoderFile,args.sample_data)
+    X_train = df0.iloc[:, :-1].values
+    y_train = df0.iloc[:, -1].values
     adbosstregressor0 = Eval(X_train, y_train, AdaBoostRegressor(random_state=args.random_state, n_estimators=args.n_estimators))
     adbosstregressor0.Train()
     _ = adbosstregressor0.predict()
@@ -36,9 +37,7 @@ if __name__ == '__main__':
     print('\n')
 
     # Synthetic data
-    args.sample_data = args.gendata_dir
-    df1 = preprocessingFirstColumn(args)
-    #df1 = pd.read_csv(args.gendata_dir)
+    df1 = preprocessingFirstColumnSimilar_train(args.label_encoderFile, args.onehot_encoderFile, args.gendata_dir)
     Xmm = df1.iloc[:, :-1].values
     ymm = df1.iloc[:, -1].values
     adbosstregressor = Eval(Xmm, ymm, AdaBoostRegressor(random_state=args.random_state, n_estimators=args.n_estimators))
@@ -59,6 +58,11 @@ if __name__ == '__main__':
     print('Mean Squared Error of the combined original and generated synthetic data modelled with Adaboost Regressor = {0}'.format(ARcom.MSE()))
     print('Root Mean Squared Error of combined original and generated synthetic data modelled with Adaboost Regressor = {0}'.format(np.sqrt(ARcom.MSE())))
     print('Explained Variance Score of combined original and generated synthetic data modelled with Adaboost Regressor = {0}'.format(ARcom.Explained_Variance_Score()))
+
+
+
+
+
 
 
 
